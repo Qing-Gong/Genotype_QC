7b043bdac5b0f40693a277f4a69b31eb482f27e5
Qing-Gong <qgong@luc.edu>
2024-09-26 11:18:23 -0500

    filtering

--- 3a_MD_sex_race.R
+++ 3a_MD_sex_race.R
@@ -3,19 +3,19 @@ library(readxl);library(tidyverse);library(stringr)
 # AMR-AFR
 #Read in sex and race information
 
-# Clinical MOD records  #from Rebecca
-MOD_clinical <- read_excel("MOD_subject_sex_race_info_7.7.23.xlsx") %>%   
+# Clinical MD records 
+MD_clinical <- read_excel("sex_race_info_23.xlsx") %>%   
   mutate(sex=ifelse(Sex == 'Female', 2, 1)) 
 
-# PRISM info from Rebecca
-PRISM <- read.csv("PRISM to MOD-PRISM id mapping_from MS 3.17.18 w MS sasmple ID info added.csv") %>% 
-  rename(PRISM.ID=1,
+# PM info 
+PM <- read.csv("PM.csv") %>% 
+  rename(PM.ID=1,
          mom=3,
          baby=4,
          Sex=6) 
 
-# GOH info from Emma
-GOH1 <- read.csv("MOD_OBER_Plate7_et.csv")[1:21, ] %>% 
+# GH info
+GH1 <- read.csv("Plt7.csv")[1:21, ] %>% 
   mutate(ID=paste0(Participant.ID, "_p7")) %>% 
   mutate(sex=ifelse(Sex == 'Female', 2, 1)) %>% 
   mutate(race=ifelse(str_starts(Race, "Black"), 'Black', 
@@ -23,15 +23,15 @@ GOH1 <- read.csv("MOD_OBER_Plate7_et.csv")[1:21, ] %>%
                             ifelse(Race == "AA", "Black", Race)))) %>% 
   select(7:9)
 
-# GOH info from NATHAN
-GOH2 <- read.table("ROBIs_Genotyped_052023_update.csv", header = TRUE) %>% 
+# GH info
+GH2 <- read.table("R_update.csv", header = TRUE) %>% 
   mutate(ID=paste0(ID, "_p14")) %>% 
   mutate(sex=ifelse(Sex == 'Female', 2, 1)) %>% 
   rename(race=3) %>% 
   select(1, 4, 3)
 
-# NUAA info from Chris
-NUAA <- read_csv("Inventory of Q576R Genotyped NU DNA to UC 8-9-21.csv", show_col_types = FALSE) %>%
+# NAA info
+NAA <- read_csv("INU.csv", show_col_types = FALSE) %>%
   rename(uc_id=1) %>% 
   mutate(ID=paste0(uc_id, '_p9')) %>%
   mutate(sex=ifelse(Sex == 'Female', 2, 1)) %>% 
@@ -42,28 +42,28 @@ NUAA <- read_csv("Inventory of Q576R Genotyped NU DNA to UC 8-9-21.csv", show_co
 #Add sex and race to samples with 5 digits ID
 
 # Samples in AMR-AFR ----- 769
-mod2123ID <- read.table("mod2123.list")   #
+MD2123ID <- read.table("MD2123.list")   #
 
-# Add sex to samples with *****MOD --5_digit ID
-mod5dig <- mod2123ID %>% 
+# Add sex to samples with *****MD --5_digit ID
+md5dig <- MD2123ID %>% 
   rename(ID=V1) %>% 
   filter(str_detect(ID, '^[1-9]')) %>% 
   filter(nchar(ID) > 7) %>% 
   mutate(id = str_sub(ID, 1, 7)) %>% 
-  left_join(MOD_clinical, by=c('id'='MOD_ID')) %>% 
+  left_join(MD_clinical, by=c('id'='MD_ID')) %>% 
   rename(race=Race) 
 
-# Add sex and race info for 13807 appeared in the Alternative_ID column
-mod5dig$sex[mod5dig$id == '13807-I'] <- 1    # 13807-I male
-mod5dig$sex[mod5dig$id == '13807-M'] <- 2    # 13807-M female
-mod5dig$race[str_starts(mod5dig$id, '13807-')] <- 'Black'    # 13807-M female
+# Add sex and race info for XXXXX appeared in the Alternative_ID column
+md5dig$sex[md5dig$id == 'XXXXX-I'] <- 1    # XXXXX-I male
+md5dig$sex[md5dig$id == 'XXXXX-M'] <- 2    # XXXXX-M female
+md5dig$race[str_starts(md5dig$id, 'XXXXX-')] <- 'Black'    # XXXXX-M female
 
-mod5d <- mod5dig %>% 
+md5d <- md5dig %>% 
   select(1, 9, 7)
 
-# Add sex and race to PRISM samples
+# Add sex and race to PM samples
 
-mom <- subset(PRISM, mom!="") %>% 
+mom <- subset(PM, mom!="") %>% 
   mutate(ID=paste0(mom, "-M")) %>% 
   mutate(sex=2,
          id=mom) %>% 
@@ -71,18 +71,18 @@ mom <- subset(PRISM, mom!="") %>%
   select(9,7,8,10)
 
 
-baby <- subset(PRISM, baby!="") %>% 
+baby <- subset(PM, baby!="") %>% 
   mutate(ID=paste0(baby, "-I")) %>% 
   mutate(sex=ifelse(Sex == 'F', 2, 1),
          id=baby) %>% 
   mutate(race=ifelse(Race=='African-American', 'Black', Race)) %>% 
   select(9, 7, 8, 10)
 
-write.csv(mom, 'EFZmom_sex_race_AFR.csv', row.names = FALSE, quote = FALSE)
-write.csv(baby, 'EFZbaby_sex_race_AFR.csv', row.names = FALSE, quote = FALSE)
+write.csv(mom, 'EZmom_sex_race_AFR.csv', row.names = FALSE, quote = FALSE)
+write.csv(baby, 'EZbaby_sex_race_AFR.csv', row.names = FALSE, quote = FALSE)
 
-EFZ <- mod2123ID %>% 
-  filter(str_starts(V1, 'EFZ')) %>% 
+EZ <- MD2123ID %>% 
+  filter(str_starts(V1, 'EZ')) %>% 
   mutate(ID=str_sub(V1, 1, 10)) %>% 
   left_join(rbind(mom, baby), by=c('ID'='id')) %>% 
   select(1, 4, 5) %>% 
@@ -90,24 +90,23 @@ EFZ <- mod2123ID %>%
 
 # Add sex and race to GOH
 
-GOH <- mod2123ID %>% 
-  inner_join(rbind(GOH1, GOH2),by=c('V1'='ID')) %>% 
+GH <- MD2123ID %>% 
+  inner_join(rbind(GH1, GH2),by=c('V1'='ID')) %>% 
   rename(ID=V1)
 
 
-# Add sex and race to NAPs and dp/db samples   
+# Add sex and race to dp/db samples   
 
-#Checked all cell line and dTL samples female and Black like NAPs
-#https://mnlab.uchicago.edu/mod/genotyping/E1-E10/full.html
+#Checked all cell line and XX samples female and Black
 
-mod_nuaa_prism_goh <- rbind(mod5d, NUAA, EFZ, GOH)
+mnpg <- rbind(md5d, NAA, EZ, GH)
 
-MOD_sex_race <- mod2123ID %>% 
-  left_join(mod_nuaa_prism_goh, by=c('V1'='ID')) %>%
+MD_sex_race <- MD2123ID %>% 
+  left_join(mnpg, by=c('V1'='ID')) %>%
   mutate(Sex=ifelse(is.na(sex), 2, sex)) %>% 
   mutate(Race=ifelse(is.na(race), 'Black', race)) %>% 
   rename(ID=V1) %>% 
   select(1, 4,5)
 
-write.csv(MOD_sex_race, 'MOD_sex_race_AFR.csv', row.names = FALSE, quote = FALSE)
+write.csv(MD_sex_race, 'MD_sex_race_AFR.csv', row.names = FALSE, quote = FALSE)
 
